b96ca3c3ba5800f2e727977629770fb3ad97b3e9
Tom Hardwicke <hardwickete@gmail.com>
2021-01-04 10:24:32 +0100

    proof revisions and style formatting

--- analysis/meta_analysis.R
+++ analysis/meta_analysis.R
@@ -32,15 +32,17 @@ presentStudy_totalN <- articleLevelSummary %>%
   sum()
 
 ## get the number of successful cases prior to author assistance
-presentStudy_preAuthorSuccess_n <- articleLevelSummary %>% 
+presentStudy_preAuthorSuccess_n <- articleLevelSummary %>%
   filter(finalOutcome == "Reproducible\nwithout author\ninvolvement") %>%
   pull(n)
 
 ## get the number of successful cases after author assistance
-presentStudy_postAuthorSuccess_n <- articleLevelSummary %>% 
-  filter(finalOutcome %in% 
-           c("Reproducible\nwithout author\ninvolvement",
-             "Reproducible\nwith author\ninvolvement")) %>%
+presentStudy_postAuthorSuccess_n <- articleLevelSummary %>%
+  filter(finalOutcome %in%
+    c(
+      "Reproducible\nwithout author\ninvolvement",
+      "Reproducible\nwith author\ninvolvement"
+    )) %>%
   pull(n) %>%
   sum()
 
@@ -50,15 +52,17 @@ presentStudy_postAuthorSuccess_n <- articleLevelSummary %>%
 ma_data_pre <- data.frame(
   type = c("Before author contact", "Before author contact"),
   study = c("Hardwicke et al. (2018)", "Present study"),
-  cases = c(hardwicke2018_preAuthorSuccess_n, presentStudy_preAuthorSuccess_n), 
-  total = c(hardwicke2018_totalN,presentStudy_totalN))
+  cases = c(hardwicke2018_preAuthorSuccess_n, presentStudy_preAuthorSuccess_n),
+  total = c(hardwicke2018_totalN, presentStudy_totalN)
+)
 
 ## ...one for post-author assistance
 ma_data_post <- data.frame(
   type = c("After author contact", "After author contact"),
   study = c("Hardwicke et al. (2018)", "Present study"),
-  cases = c(hardwicke2018_postAuthorSuccess_n, presentStudy_postAuthorSuccess_n), 
-  total = c(hardwicke2018_totalN, presentStudy_totalN))
+  cases = c(hardwicke2018_postAuthorSuccess_n, presentStudy_postAuthorSuccess_n),
+  total = c(hardwicke2018_totalN, presentStudy_totalN)
+)
 
 ## ...and one for both together
 ma_data_combined <- rbind(ma_data_pre, ma_data_post)
@@ -69,7 +73,7 @@ ma <- metaprop(
   event = cases, # specify column of reproducible cases
   n = total, # specify column of total cases
   studlab = study, # specify column of study identity
-  method.ci = 'WSCC', # compute Wilson confidence intervals
+  method.ci = "WSCC", # compute Wilson confidence intervals
   comb.fixed = F, # don't used fixed effects model
   comb.random = T, # use random effects model
   sm = "PRAW", # specify summary measure as raw proportions
@@ -83,23 +87,23 @@ ma <- metaprop(
 
 ## build forest plot
 
-#pdf(file="forestPlot.pdf", width = 12, height = 6) # only if saving to pdf
+# pdf(file="forestPlot.pdf", width = 12, height = 6) # only if saving to pdf
 
 forest(ma, # supply meta-analysis object from above
-       leftlabs = c("Study", "Reproducible\ncases", "Total\ncases"),
-       rightlabs = c("Proportion", "[95% CI]"),
-       xlab = 'Proportion',
-       xlim = c(0,1),
-       col.square = 'black',
-       col.diamond = 'black',
-       col.by = 'black',
-       hetstat = F, # turn off heterogeneity stats
-       spacing = 1.25,
-       fs.heading = 12.5,
-       fontsize = 11,
-       weight.study = 'random',
-       weight.subgroup = 'weight',
-       colgap.forest.left = unit(0.5, 'cm')
+  leftlabs = c("Study", "Reproducible\ncases", "Total\ncases"),
+  rightlabs = c("Proportion", "[95% CI]"),
+  xlab = "Proportion",
+  xlim = c(0, 1),
+  col.square = "black",
+  col.diamond = "black",
+  col.by = "black",
+  hetstat = F, # turn off heterogeneity stats
+  spacing = 1.25,
+  fs.heading = 12.5,
+  fontsize = 11,
+  weight.study = "random",
+  weight.subgroup = "weight",
+  colgap.forest.left = unit(0.5, "cm")
 )
 
-#dev.off() # only if saving to pdf
+# dev.off() # only if saving to pdf
